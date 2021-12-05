8dc87f2bf253b7e5bb81cdec4f50a937a5cb1aad
zacksgamerz <81515012+zacksgamerz@users.noreply.github.com>
2021-12-05 11:12:16 +0700

    update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -8,17 +8,25 @@ import openfl.Lib;
 import openfl.display.FPS;
 import openfl.display.Sprite;
 import openfl.events.Event;
+#if android
+import lime.system.System;
+#end
 
 class Main extends Sprite
 {
 	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
-	var initialState:Class<FlxState> = Cache; // The FlxState the game starts with.
+	var initialState:Class<FlxState> = TitleState; // The FlxState the game starts with.
 	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
 	var framerate:Int = 60; // How many frames per second the game should run at.
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
 	public static var fpsVar:FPS;
+
+	#if (mobileC || android)
+	public static var path = lime.system.System.applicationStorageDirectory;
+	#end
+
 
 	// You can pretty much ignore everything from here on - your code should go in your states.
 
@@ -66,21 +74,19 @@ class Main extends Sprite
 		}
 
 		#if !debug
-		initialState = Cache;
+		initialState = TitleState;
 		#end
-
-		Paths.getModFolders();
-		ClientPrefs.startControls();
+		
+		ClientPrefs.loadDefaultKeys();		
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
+		var ourSource:String = "assets/videos/DO NOT DELETE OR GAME WILL CRASH/dontDelete.webm";
 
-		#if !mobile
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
 		if(fpsVar != null) {
 			fpsVar.visible = ClientPrefs.showFPS;
 		}
-		#end
-
+	
 		#if html5
 		FlxG.autoPause = false;
 		FlxG.mouse.visible = false;
