5baa6d410f94a9af1845ab01c23e8aea7fb26718
zacksgamerz <81515012+zacksgamerz@users.noreply.github.com>
2021-12-06 23:49:34 +0700

    update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2349,7 +2349,7 @@ class PlayState extends MusicBeatState
 		}
 		botplayTxt.visible = cpuControlled;
 
-		if (FlxG.keys.justPressed.ENTER && startedCountdown && canPause)
+		if (FlxG.keys.justPressed.ENTER #if android || FlxG.android.justPressed.BACK #end && startedCountdown && canPause)
 		{
 			var ret:Dynamic = callOnLuas('onPause', []);
 			if(ret != FunkinLua.Function_Stop) {
