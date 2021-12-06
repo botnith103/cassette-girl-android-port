7ac1302bf18161c01b619a35928e7845064424bc
zacksgamerz <81515012+zacksgamerz@users.noreply.github.com>
2021-12-06 23:50:37 +0700

    update OptionsState.hx

--- source/OptionsState.hx
+++ source/OptionsState.hx
@@ -45,7 +45,7 @@ using StringTools;
 // TO DO: Redo the menu creation system for not being as dumb
 class OptionsState extends MusicBeatState
 {
-	var options:Array<String> = ['Notes', #if windows'Controls',#elseif mobileC 'Mobile Controls',#end 'Preferences'];
+	var options:Array<String> = ['Notes', 'Keyboard Controls', 'Mobile Controls', 'Preferences'];
 	private var grpOptions:FlxTypedGroup<Alphabet>;
 	private static var curSelected:Int = 0;
 	public static var menuBG:FlxSprite;		
